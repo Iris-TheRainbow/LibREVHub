ec8e382ba938fe4ac3c99762bfffbd52de9d7fe2
iris <snazzsinclair@gmail.com>
2025-06-10 02:30:36 -0500

    coms rewrite v93, also multiprocessing for it

--- Coms/LynxCom.py
+++ Coms/LynxCom.py
@@ -10,6 +10,8 @@ import time
 
 MAX_COM_ATTEMPTS = 5
 MAX_BYTE_WAIT_TIME_SECONDS = .01
+MAX_RESPONSE_WAIT_TIME_SECONDS = 1
+
 class comPort:
     def __init__(self, sn, name):
         self.sn = sn
@@ -34,9 +36,17 @@ class LynxCom:
 
         self.sendingManager = multiprocessing.Manager()
         self.sentPackets = sendingManager.list()
+        self.packetSendQueue = multiprocessing.Queue()
+        self.packetMessageNumberQueue = multiprocessing.Queue()
+        self.packetSendingProcess = multiprocessing.Process(target=self.sendPacketWorker, args=(packetSendQueue, sentPackets, packetMessageNumberQueue))
 
         self.receivingManager = multiprocessing.Manager()
         self.receivedPackets = receivingManager.list()
+        self.packetReceivingProcess = multiprocessing.Process(target=self.recievingWorker, args=(receivedPackets, sentPackets))
+
+        self.packetReceivingProcess.start()
+        self.packetSendingProcess.start()
+        
 
     def setActivePort(self, port: str) -> None:
         self.serialProcessor.setPort(port)
@@ -55,25 +65,30 @@ class LynxCom:
                             comPorts.append(comPort(serialNumber, usbDevice.device))          
   
         return comPorts
+
     def safeReadBytes(self, numberOfBytes: int = 1):
         byteList = []
-        bytesLeft = numberOfBytes
-        timeout = false
+        start_time = 0
+
         while len(byteList) < numberOfBytes:
-            if self.serialProcessor.in_waiting() > 1:
-                byteList.append(str(binascii.hexlify(self.serialProcessor.read(1)).upper())[2:-1])
-            elif timeout: 
-                break
-        if not len(byteList) == numberOfBytes:
-            RuntimeWarning("attempted to read more bytes than were available to read")
+            if self.serialProcessor.in_waiting() >= 1:
+                byteList.append(binascii.hexlify(self.serialProcessor.read(1)).upper().decode())
+                start_time = 0
+            else:
+                if start_time == 0:
+                    start_time = time.time()
+                elif time.time() - start_time > self.MAX_BYTE_WAIT_TIME_SECONDS:
+                    break
+                time.sleep(0.0001)
+
+        if len(byteList) != numberOfBytes:
+            warnings.warn("attempted to read more bytes than were available to read")
         return byteList
 
-    def recievingWorker(self, recievingList: ListProxy[LynxMessage.LynxPacket]) -> None:
+    def recievingWorker(self, recievingList: ListProxy[LynxMessage.LynxPacket], sentList: ListProxy[LynxMessage.LynxPacket]) -> None:
         while true:
             byteList = []
-            fullPacket = False
             packetLength = 0
-
             if self.serialProcessor.in_waiting() > 0:
                 if safeReadBytes() == "44":
                     if safeReadBytes() == "4B":
@@ -84,25 +99,64 @@ class LynxCom:
                     if self.serialProcessor.in_waiting() > packetLength - 4:
                         byteList += safeReadBytes(packetLength - 4)
                         fullPacket = True
-                        recievingList.append(self.parseBytes(bytePacket))
+                        packet = self.parseBytesIntoPacket(bytePacket)
+                        for i in range(sentList):
+                            if packet.msgNum == packet.header.msgNum:
+                                sentList.pop(i)
+                        recievingList.append(packet)
+                        
             else:
                 sleep(.001)
-            
+
+    def getResponse(self, messageNumber: int) -> LynxMessage.LynxPacket:
+        startTime = time.time_ns/(10**9)
+        timeout = False
+        while True and not timeout:
+            for packet in self.receivedPackets:
+                if packet.header.msgNum == messageNumber:
+                    return packet
+            if time.time_ns/(10**9) > startTime + MAX_RESPONSE_WAIT_TIME_SECONDS:
+                break
+            sleep(.00001)
+
+    def sendPacketWorker(self, packetQueue: multiprocessing.queues.Queue, packetMessageNumberQueue: multiprocessing.queues.Queue, packetList: ListProxy[LynxMessage.LynxPacket]) -> None:
+        while True:
+            if not packetQueue.empty():
+                PACKET = packetQueue.get()
+                if not isinstance(packet, LynxMessage.LynxPacket): RuntimeError("Attempted to send something other than a LynxPacket")
+                attempts = 0;
+                self.messageCount = (self.messageCount + 1)  % 256
+                if self.messageCount == 0: self.messageCount += 1
+                packetMessageNumberQueue.put(self.messageCount)
+                packet.header.messageCount = self.messageCount
+                self.sentPackets.append(packet)
+                while attempts < MAX_COM_ATTEMPTS:
+                        try:
+                            self.serialProcessor.write(binascii.unhexlify(packet.getPacketData()))
+
+                        except serial.SerialException as e:
+                            attempts += 1
+                            RuntimeWarning("Failed to write packet")
+            else:
+                sleep(.001)
+
+    def sendPacket(self, packet: LynxMessage.LynxPacket, destination: int) -> int:
+        packetToSend = packet
+        packetToSend.header.destination = destination
+        self.packetSendQueue.put(packetToSend)
+        while self.packetMessageNumberQueue.empty():
+            sleep(.00001)
+        return self.packetMessageNumberQueue.get()
             
     def discovery(self) -> None:
         discoveryPacket = LynxMessage.Discovery()
-        self.sendPacketBlocking(discoveryPacket, 255)
-        for packet in self.recieveBlocking():
-            LynxDevices.LynxModes.append(LynxModule(self, packet.header.source, packet.payload.parent))
-    
-    def checkMsgNumber(self, incomingPacket: LynxMessage.LynxPacket) -> bool:
-        for i in range(self.sentPackets):
-            if packet.header.msgNum == incomingPacket.header.msgNum:
-                self.sentPackets.pop(i)
-                return True
-        return False
+        self.sendPacketInstant(discoveryPacket, 255)
 
-    def parseBytes(self, bytes: list[str]) -> LynxMessage.LynxPacket:
+        for packet in self.getResponse():
+            LynxDevices.LynxModes.append(LynxModule(self, packet.header.source, packet.payload.parent))
+
+
+    def parseBytesIntoPacket(self, bytes: list[str]) -> LynxMessage.LynxPacket:
         if isValidChecksum(bytes[:-2], int(bytes[-2:], 16)): 
             packetLength = int(self.swapEndianess(bytes[LynxMessage.LynxPacket.LengthIndex_Start:LynxMessage.LynxPacket.LengthIndex_End]), 16)
             packetDest = int(bytes[LynxMessage.LynxPacket.DestinationIndex_Start:LynxMessage.LynxPacket.DestinationIndex_End], 16)
@@ -127,22 +181,6 @@ class LynxCom:
                 bytePointer = len(payloadMember) * 2
 
         return newPacket
-
-    def sendPacketBlocking(self, packet: LynxMessage.LynxPacket, destinationModule: int) -> None:
-        if not isinstance(packet, LynxMessage.LynxPacket): RuntimeError("Attempted to send something other than a LynxPacket")
-        attempts = 0;
-        packet.header.destination = destinationModule
-        self.messageCount = (self.messageCount + 1)  % 256
-        if self.messageCount == 0: self.messageCount += 1
-        packet.header.messageCount = self.messageCount
-        self.sentPackets.append(packet)
-        while attempts < MAX_COM_ATTEMPTS:
-                try:
-                    self.serialProcessor.write(binascii.unhexlify(packet.getPacketData()))
-
-                except serial.SerialException as e:
-                    attempts += 1
-                    RuntimeWarning("Failed to write packet")
 
     def isValidChecksum(self, incomingPacket: list[str], receivedChkSum: int):
         calcChkSum = 0
